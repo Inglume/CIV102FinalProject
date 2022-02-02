6344fe70a2cb2d4fb30fde776aa3e080dc24b7f7
Inglume <inglume@gmail.com>
2022-02-01 22:01:03 -0500

    Fixed some stuff

--- civ102calculations.m
+++ civ102calculations.m
@@ -8,45 +8,35 @@ BMD = zeros(1, n); % Initialize SFD(x)
 %% 1. Point Loading Analysis (SFD, BMD)
 
 P = 1; % CHANGE THIS probapbly or do some loop
-SFD = zeros(1, n);
-BMD = zeros(1, n);
-%[SFD2L, BMD2L] = ApplyTwoLoads(1, x, SFD, BMD);
-%PlotDiagrams(x, L, SFD_2L, BMD_2L)
+SFD2L = zeros(1, n);
+BMD2L = zeros(1, n);
+[SFD2L, BMD2L] = ApplyTwoLoads(1, x, SFD, BMD);
+%PlotDiagrams(x, L, SFD2L, BMD2L)
 
-trainLength = 856;
-t = zeros(0, trainLength);
-%trainSFD = zeros(1, trainLength + 1);
-%trainBMD = zeros(1, trainLength + 1);
-[SFDTrain, BMDTrain] = ApplyTrainLoad(x, SFD, BMD);
 
-%PlotDiagrams(x, L, SFDTrain, BMDTrain)
-PlotTrain(x, L, SFDTrain, BMDTrain)
+%[SFDTrain, BMDTrain] = ApplyTrainLoad(x, SFD, BMD);
+%PlotTrain(x, L, SFDTrain, BMDTrain)
+
 
 %% 2. Define cross-sections
-% There are many (more elegant ways) to construct cross-section objects
 
-GeometricInputs = [];
-
-xc = [0 550 L]; % Location, x, of cross-section change
-bft = [100 100 100]; % Top Flange Width
-tft = [2.54 2.54 2.54]; % Top Flange Thickness
-hw = [100 120 100]; % Web Height
-tw = [1.27 1.27 1.27]; % Web Thickness (Assuming 2 separate webs)
-bfb = [80 80 80]; % Bottom Flange Width
-tfb = [1.27 1.27 1.27]; % Bottom Flange Thickness
-a = [400 400 400]; % Diaphragm Spacing
+%xc = [0 550 L]; % Location, x, of cross-section change
+%bft = [100 100 100]; % Top Flange Width
+%tft = [2.54 2.54 2.54]; % Top Flange Thickness
+%hw = [100 120 100]; % Web Height
+%tw = [1.27 1.27 1.27]; % Web Thickness (Assuming 2 separate webs)
+%bfb = [80 80 80]; % Bottom Flange Width
+%tfb = [1.27 1.27 1.27]; % Bottom Flange Thickness
+%a = [400 400 400]; % Diaphragm Spacing
 % have to find a way to deal with diaphragms (or not)
-
-% format geometric inputs in some sort of way
-% each column corresponds to different cross section
-% tried implementing in another way, wehre each row corresponds to
-% different cross section
 
 %GeometricInputs(end + 1, :) = [0, 100, 2.54, 100, 1.27, 80, 1.27, 400]; % add something for length of glue tabs on top and bottom (thickness should be same as thickness of webs
 %GeometricInputs(end + 1, :) = [550, 100, 2.54, 120, 1.27, 80, 1.27, 400];
 %GeometricInputs(end + 1, :) = [L, 100, 2.54, 100, 1.27, 80, 1.27, 400];
 
 % Design 0
+GeometricInputs = [];
+
 GeometricInputs(end + 1, :) = [0, 100, 1.27, 72.46, 1.27, 80, 1.27, 30];
 GeometricInputs(end + 1, :) = [30, 100, 1.27, 72.46, 1.27, 80, 1.27, 520];
 GeometricInputs(end + 1, :) = [550, 100, 1.27, 72.46, 1.27, 80, 1.27, 30];
@@ -55,6 +45,14 @@ GeometricInputs(end + 1, :) = [1060, 100, 1.27, 72.46, 1.27, 80, 1.27, 30];
 GeometricInputs(end + 1, :) = [1090, 100, 1.27, 72.46, 1.27, 80, 1.27, 160];
 GeometricInputs(end + 1, :) = [1280, 100, 1.27, 72.46, 1.27, 80, 1.27, 30];
 GeometricInputs(end + 1, :) = [L, 100, 1.27, 72.46, 1.27, 80, 1.27, 30];
+
+% Design 1.0 woowwowow
+%{
+GeometricInputs = [];
+
+%GeometricInputs(end + 1, :) = [xc, bft, tft, hw, tw, bfb, tfb, a];
+insert more stuff here
+%}
 
 % Optional but you need to ensure that your geometric inputs are correctly implemented
 % VisualizeBridge( {CrossSectionInputs} );
@@ -68,14 +66,18 @@ mu = 0.2;
 
 CrossSectionProperties = SectionProperties(GeometricInputs, n);
 
-
 % find a way to automate this for ease of use
+
+for i = 1 : size(GeometricInputs, 1) - 1
+    cs = CrossSectionProperties(GeometricInputs(i, 1) + 1, :);
+    sprintf("Cross Section @ %d mm - ybot: %.3g mm ytop: %.3g mm I: %.3g mm^4 Q: %d", GeometricInputs(i, 1), cs(8:11))
+end
+    
 %cs1 = CrossSectionProperties(1, :);
 %cs2 = CrossSectionProperties(551, :);
 
 %sprintf("Cross Section 1 - ybot: %.3g mm ytop: %.3g mm I: %.3g mm^4 Q: %d", cs1(8:11))
 %sprintf("Cross Section 2 - ybot: %.3g mm ytop: %.3g mm I: %.3g mm^4 Q: %d", cs2(8:11))
-
 
 %{
 %% 4. Calculate Failure Moments and Shear Forces
@@ -161,10 +163,6 @@ function [trainSFD, trainBMD] = ApplyTrainLoad(x, SFD, BMD)
         trainSFD(i, :) = SFD;
         trainBMD(i, :) = BMD;
     end
-    %trainSFD = SFD;
-    %trainBMD = BMD;
-
-    %PlotTrain(x, trainSFD, trainBMD)
 end
 
 function [SFD, BMD] = UpdateDiagrams(xP, P, x, SFD, BMD)
@@ -233,33 +231,6 @@ function PlotTrain (x, L, trainSFD, trainBMD) % include curvature diagram
     set(gcf, 'Name', 'Force and Moment Diagrams') % name of window
 end
 
-function PlotTrain2 (x, L, SFD, BMD)
-    trainLength = 856;
-    t = tiledlayout(2,1);
-    ax1 = nexttile;
-    xlim([0 L])
-    xlabel(ax1, "x (mm)")
-    ylabel(ax1, "V (kN)")
-    title(ax1, "Shear Force Diagram")
-    ax1.XAxisLocation = 'origin';
-    hold(ax1, 'on')
-    ax2 = nexttile;
-    xlim([0 L])
-    xlabel(ax2, "x (mm)")
-    ylabel(ax2, "M (kN m)")
-    title(ax2, "Bending Moment Diagram")
-    ax2.XAxisLocation = 'origin';
-    set(ax2, 'YDir','reverse')
-    hold(ax2, 'off')
-
-    for i = 1 : L - size(SFD, 2)
-        plot(ax1, x(i : trainLength + i + 1), SFD)
-        plot(ax2, x(i : trainLength + i + 1), BMD)
-    end
-
-    title(t, "Diagrams for Train Loading Scenario")
-end
-
 function VisualizeBridge(GeometricInputs)
 % Optional. Provides a graphical interpretation of user geometric inputs
 end
@@ -269,46 +240,59 @@ function CrossSectionProperties = SectionProperties(GeometricInputs, n) % includ
 % Calculates important sectional properties. Including but not limited to ybar, I, Q, etc.
 %   Input: Geometric Inputs. Format will depend on user
 %   Output: Sectional Properties at every value of x. Each property is a 1-D array of length n
-    sect = zeros(n, 4); % columns: ybot, ytop, I, Q
+    sect = zeros(n, 4); % columns: ybot, ytop, I, Qmax (yfla, yweb, )
     geom = zeros(n, size(GeometricInputs, 2) - 1);
     for i = 1 : (size(GeometricInputs, 1) - 1) % get row # (for each cross section)
-        areas = zeros(3, 1); % length three (top, web, bottom)
-        distances = zeros(3, 1); % length three (distance from bot)
-        secondMomInert = zeros(3, 1); % second moment of inertias for each part
+        areas = zeros(1, 3); % length three (top, web, bottom)
+        distances = zeros(1, 3); % length three (distance from bot)
+        secondMomInert = zeros(1, 3); % second moment of inertias for each part
         areas(1) = GeometricInputs(i, 2) * GeometricInputs(i, 3);
         areas(2) = GeometricInputs(i, 5) * GeometricInputs(i, 4);
         areas(3) = GeometricInputs(i, 6) * GeometricInputs(i, 7);
         distances(3) = GeometricInputs(i, 7) / 2;
-        distances(2) = GeometricInputs(i, 4) / 2 + distances(3);
-        distances(1) = GeometricInputs(i, 3) / 2 + distances(2);
+        distances(2) = GeometricInputs(i, 4) / 2 + GeometricInputs(i, 7);
+        distances(1) = GeometricInputs(i, 3) / 2 + GeometricInputs(i, 4) + GeometricInputs(i, 7);
         secondMomInert(1) = (GeometricInputs(i, 2) * GeometricInputs(i, 3) ^ 2) / 12;
-        secondMomInert(2) = (GeometricInputs(i, 4) * GeometricInputs(i, 5) ^ 2) / 12;
+        secondMomInert(2) = (GeometricInputs(i, 5) * GeometricInputs(i, 4) ^ 2) / 12;
         secondMomInert(3) = (GeometricInputs(i, 6) * GeometricInputs(i, 7) ^ 2) / 12;
         ybot = 0; % sum of area times relative y divided by sum of area
         ytop = 0; % ybar from top
         I = 0; % sum of individual i plus area times distance squared
         yfla = 0; % distance from global centroid to centroid of flange above ybar
         yweb = 0; % distance from global centroid to centroid of web above ybar
-        Q = 0; % sum of area times distance
+        ytab = 0;
+        Qmax = 0; % sum of area times distance
         for k = 1 : 3
-                ybot = ybot + areas(k) * distances(k);
+            ybot = ybot + areas(k) * distances(k);
+            if k == 2
+                ybot = ybot + areas(k) * distances(k); % im lazy so yea
+                ybot = ybot + 20 * 1.27 * (distances(1) - GeometricInputs(i , 3) / 2 - 1.27 / 2);
+            end
         end
-        ybot = ybot / sum(areas);
+        ybot = ybot / (sum(areas) + areas(2) + 20 * 1.27); % hardcode glue tabs cause I am in pain
         ytop = GeometricInputs(i, 3) + GeometricInputs(i, 4) + GeometricInputs(i, 7) - ybot;
-        for k = 1 : 3
-                I = I + secondMomInert(k) + areas(k) * (distances(k) - ybot) ^ 2; % prbaobly somethign wrong with this calc
-                % Q = Q + areas(k) * (distances(k) - ybot); % do Q just for areas above centroid
-        end
         yfla = distances(1) - ybot;
         yweb = (GeometricInputs(i, 4) - ybot + GeometricInputs(i, 7)) / 2;
-        Q = areas(1) * yfla;
-        Q = Q + GeometricInputs(i, 5) * 2 * yweb ^ 2; % multiply thickness of web by height (yweb * 2), multiply by distance (yweb)
+        ytab = yfla - 1.27 / 2;
+        for k = 1 : 3
+            I = I + secondMomInert(k) + areas(k) * (distances(k) - ybot) ^ 2; % prbaobly somethign wrong with this calc
+            if k == 2
+                I = I + secondMomInert(k) + areas(k) * yweb ^ 2; % lazy * 2
+                I = I + (20 * 1.27 ^ 3) / 12 + 20 * 1.27 * ytab ^ 2; % hardcode glue tabs again
+            end
+            % Q = Q + areas(k) * (distances(k) - ybot); % do Q just for areas above centroid
+        end
+
+        Qmax = areas(1) * yfla;
+        Qmax = Qmax + GeometricInputs(i, 5) * 4 * yweb ^ 2; % multiply 2 times thickness of web by height (yweb * 2), multiply by distance (yweb)
+        Qmax = Qmax + 20 * 1.27 * ytab;
         for j = (GeometricInputs(i, 1) + 1) : GeometricInputs(i + 1, 1) + 1 % beginning of current cross section to beginning of next cross section
             geom(j, :) = GeometricInputs(i, 2 : end);
             sect(j, 1) = ybot;
             sect(j, 2) = ytop;
             sect(j, 3) = I;
-            sect(j, 4) = Q;
+            sect(j, 4) = Qmax;
+            % yfla, yweb, ytab
         end
     end
     CrossSectionProperties = [geom sect];
@@ -318,10 +302,10 @@ function [V_fail] = Vfail(SectionalProperties, TauU)
 % Calculates shear forces at every value of x that would cause a matboard shear failure
 %   Input: Sectional Properties (list of 1-D arrays), TauU (scalar material property)
 %   Output: V_fail a 1-D array of length n
- I = {SectionalProperties};
- b = {SectionalProperties};
-Qcent = {SectionalProperties};
- V_fail = TauU .* I .* b ./ Qcent;
+    I = {SectionalProperties};
+    b = {SectionalProperties};
+    Qcent = {SectionalProperties};
+    V_fail = TauU .* I .* b ./ Qcent;
 end
 
 function [V_Buck] = VfailBuck(SectionalProperties, E, mu)
